dc8c6654b9d961b184ddacbba4755eabc52fc9c3
Class Account <ee125-bp@m92p-3.EECS.Berkeley.EDU>
2014-10-30 18:19:36 -0700

    created draw pkg, outlined draw_path.py

---
+++ workspace/src/draw/src/draw_path.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+# use moveit to plan and move baxter arm
+def draw_line(linesegment):
+
+
+# raise hand 2 inches from current point
+def bring_up():
+
+
+# move hand to initial point to start drawing from
+def bring_down(target_point):
+
+# planner, takes in list of line segments. Tells baxter which ones to drawing
+def plan_line_order(line_segments):
+
+
+# project picture frame coordinates to world frame coordinates
+def to_world_coordinate(single_point):
+
+# convert all image coordinates to world frame coordinates
+def to_world_coordinate(single_point):
