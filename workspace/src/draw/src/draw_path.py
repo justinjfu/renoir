e75f38c254f26c99d84a4e5d951459228aa2f0ba
Class Account <ee125-bp@m92p-3.EECS.Berkeley.EDU>
2014-10-30 18:34:43 -0700

    fixed typo

--- workspace/src/draw/src/draw_path.py
+++ workspace/src/draw/src/draw_path.py
@@ -12,7 +12,7 @@ def bring_up():
 # move hand to initial point to start drawing from
 def bring_down(target_point):
 
-# planner, takes in list of line segments. Tells baxter which ones to drawing
+# planner, takes in list of line segments. Tells baxter which ones to draw
 def plan_line_order(line_segments):
 
 
@@ -21,3 +21,4 @@ def to_world_coordinate(single_point):
 
 # convert all image coordinates to world frame coordinates
 def to_world_coordinate(single_point):
+
