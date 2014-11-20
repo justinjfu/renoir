3b8a9bb79821eef18ba1e815a2e6758f974463bc
Class Account <ee125-bv@m92p-6.EECS.Berkeley.EDU>
2014-11-20 13:27:23 -0800

    [Justin] Add ar markers subscribers

--- workspace/src/pic2world/src/run_node.py
+++ workspace/src/pic2world/src/run_node.py
@@ -8,22 +8,24 @@ from ar_track_alvar.msg import AlvarMarkers
 FIXED_MATRIX = np.array([[1,0],[0,1],[0,0]])
 
 TOPIC_NAME = 'renoir/pic2world'
+POSE_TOPIC = '/ar_pose_marker'
 
 def ar_callback(msg):
     markers = msg.markers
-    timestamp = msg.head.stamp
+    timestamp = msg.header.stamp
 
     for marker in markers:
+        print marker.id
         pose = marker.pose #geometry_msgs/PoseStamped
-        print pose
     #TODO(Justin) Update pic2world matrix
 
 def init_subscribers():
-    ar_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers.msg, ar_callback)
+    ar_sub = rospy.Subscriber(POSE_TOPIC, AlvarMarkers, ar_callback)
 
 def main():
-    rospy.init_node('renoir/pic2world_node')
+    rospy.init_node('pic2world_node')
     pub = rospy.Publisher(TOPIC_NAME, numpy_msg(Floats))
+    init_subscribers()
 
     r = rospy.Rate(10) #10Hz
     while not rospy.is_shutdown():
