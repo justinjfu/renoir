5a446ed510f3668907eee8da5906a64bb9520b2d
justinjfu <fu_kim_seacat@pepper.(none)>
2014-11-24 22:41:11 -0800

    Move baxter files to repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$THIS_FILE_DIR/workspace
+
+alias gravity='rosrun baxter_tools enable_robot.py -e'
---
+++ workspace/src/pic2world/src/fix_baxter_cam.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+rosrun baxter_tools camera_control.py -c left_hand_camera
+rosrun baxter_tools camera_control.py -o left_hand_camera -r 1280x800
+
