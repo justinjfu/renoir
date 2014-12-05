5716d93c23838e9c3c305ad3f45beb25e31bfca8
Justin Fu <justin.fu@reflektion.com>
2014-12-04 20:38:38 -0800

    Add more filters, and 3 test images

--- vectorize/vectorize.py
+++ vectorize/vectorize.py
@@ -18,19 +18,26 @@ def dist(p1, p2):
     diff = p1-p2
     return np.sqrt(np.dot(diff,diff))
 
-def read_image(filename, show=True):
+def read_image(filename, show=True, thresh = 120):
     import scipy.misc as spm
-    import pdb;pdb.set_trace()
+    #import pdb;pdb.set_trace()
     img = spm.imread(filename, flatten = True)
+    #img = spm.lena()
     img = spm.imresize(img, (100,100))
-
     img = spm.imfilter(img, ftype='find_edges')
-    img = (img > 100)
+    img = (img > thresh)
     img = img+0
+
+    #remove outer edge
+    img[:,0] = 0
+    img[:,99] = 0
+    img[0,:] =0
+    img[99,:] = 0
+
     if np.sum(img) > (100*100)/2:
         img = 1-img
     if show:
-        plt.imshow(img, interpolation='nearest', cmap='gray')
+        plt.imshow(img, interpolation='nearest', cmap='Greys')
         plt.show()
     return img
 
@@ -91,18 +98,19 @@ def select_initial(pic):
     return np.array((X[0], Y[0]))
 
 LIFT = 'lift!123'
-def generate_segments(pic):
+def generate_segments(pic, params):
     """
     Returns an iterator that plans a path through the image
     Objects in an iterator can either be a list of 2 points [[x1,y1],[x2,y2]] or an instance of LIFT
     """
+    scan_range = params.get('scan_range', 2)
     pic = np.copy(pic) #don't modify original
     while np.sum(pic) != 0:
         point = select_initial(pic)
         prev_point = None
         pic[point[0],point[1]] = 0
         while True:
-            next_point = scan_neighbors(pic, point, score_momentum(point, prev_point, K=30.0), scan_range=2)
+            next_point = scan_neighbors(pic, point, score_momentum(point, prev_point, K=30.0), scan_range=scan_range)
             if next_point is not None:
                 yield [point, next_point]
                 prev_point = point
@@ -112,18 +120,42 @@ def generate_segments(pic):
                 yield LIFT
                 break
 
+def filter_lifts(segment_iter):
+    #Filter duplicate lifts
+    lift = False
+    for segment in segment_iter:
+        if lift and segment is LIFT:
+            continue
+        lift = segment is LIFT
+        yield segment
+
+def remove_single(segment_iter):
+    last_segment = None
+    num_segments = 0
+    for segment in segment_iter:
+        if segment is not LIFT:
+            if last_segment:
+                yield last_segment
+            last_segment = segment 
+            num_segments += 1
+        else:
+            if num_segments > 1:
+                yield last_segment
+            num_segments = 0
+            last_segment = None
+            yield LIFT
+    if last_segment:
+        yield last_segment
+
 def test():
     img = np.zeros((5,5))
     for i in range(5):
         img[i,i] = 1
         img[4-i,i] = 1
 
-    img = read_image('tictactoe.png', show=True)
-    print img
-    plt.imshow(img, interpolation='nearest', cmap='gray')
-    plt.show()
+    img = read_image('cartman.jpg', show=True)
 
-    for segment in generate_segments(img):
+    for segment in filter_lifts(remove_single(generate_segments(img, {'scan_range':2}))):
         print segment
 
 def main():
